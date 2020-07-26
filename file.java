47c0347c07e4d5c442c2d0ea69275e527d3fa170
bhaskarrao1 <baipothubhaskar@gmail.com>
2020-07-26 14:27:16 +0530

    work2

--- file.java
+++ file.java
@@ -1,19 +1,5 @@
 hai i am bhaskar
->> Scripting
-Software conf. mgmt(SCM):
-....................
-SCm is a process of tracking and controlling changes in the software
 The goals of SCM are generally
-..> teamwork
-..> Defect tracking
-..> Conf. auditing
 ..> process management
 
-
-
-Git:
 >> In git everything  is a workspace
->> Every workspace will hava a copy of the repo..>all commit ids are stored only here.not outside dir/file
->> commitid(40)..>version or snapshot..> for tracking data
->> bare repo(central workspace)...>store and share
->> origin..>which we create direc
