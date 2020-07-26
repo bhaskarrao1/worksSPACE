779b3160b7b5d0df185a0da363e763c559b9303b
bhaskarrao1 <baipothubhaskar@gmail.com>
2020-07-26 14:34:58 +0530

    work3

--- file.java
+++ file.java
@@ -16,4 +16,4 @@ Git:
 >> Every workspace will hava a copy of the repo..>all commit ids are stored only here.not outside dir/file
 >> commitid(40)..>version or snapshot..> for tracking data
 >> bare repo(central workspace)...>store and share
->> origin..>which we create direc
+
