ca7a70d8b114c828f27bb9861375a8e2cd3f8d81
bhaskarrao1 <50476522+bhaskarrao1@users.noreply.github.com>
2020-07-29 11:45:31 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,6 +1,5 @@
 hai i am bhaskar
-The goals of SCM are generally
-..> process management
+The goals of SCM are generall
 >> In git everything  is a workspace
 >> Every workspace will hava a copy of the repo..>all commit ids are stored only here.not outside dir/file
 cutout chusi konni konni nameyali dude
